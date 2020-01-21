5435f99eae4086654fc270367c42f4fbc98e9e09
Vaibhav Saxena <vaibsaxena93@gmail.com>
2020-01-22 01:15:35 +0530

    rlupdate

--- img/RL/GoalDetect.cs
+++ img/RL/GoalDetect.cs
@@ -1,11 +1,6 @@
-//Detect when the orange block has touched the goal.
-//Detect when the orange block has touched an obstacle.
-//Put this script onto the orange block. There's nothing you need to set in the editor.
-//Make sure the goal is tagged with "goal" in the editor.
-
 using UnityEngine;
 
-public class GoalDetect : MonoBehaviour
+public class WindowDetect : MonoBehaviour
 {
     /// <summary>
     /// The associated agent.
@@ -13,11 +8,10 @@ public class GoalDetect : MonoBehaviour
     /// Don't need to manually set.
     /// </summary>
     [HideInInspector]
-    public PushAgentBasic agent;  //
+    public OfficeBasic agent;  //
 
     void OnCollisionEnter(Collision col)
     {
-        // Touched goal.
         if (col.gameObject.CompareTag("goal"))
         {
             agent.ScoredAGoal();
